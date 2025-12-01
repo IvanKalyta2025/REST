585c89c7c2943517bc78b01344b1a6294adcb1e3
IvanKalyta2025 <ivan25sfj@gmail.com>
2025-12-01 12:58:50 +0100

    List<TaskItem>

--- Controllers/TasksController.cs
+++ Controllers/TasksController.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace REST.Controllers
 {
-    public class TasksController
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class TasksController : ControllerBase
     {
-        
+        private static readonly List<TaskItem> tasks = new();
     }
 }
